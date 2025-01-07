e819a8fa0d1c2024c2d51b583affed463cdadcdb
HuiChihWang <taya87136@gmail.com>
2025-01-07 22:48:02 +0800

    feat: support database migration

---
+++ database/migrations/1736257248699-CreateOrderTable.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateOrderTable1736257248699 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "order"
+       (
+           "id"                 SERIAL PRIMARY KEY,
+           "renting_history_id" integer     NOT NULL,
+           "user_id"            integer     NOT NULL,
+           "order_no"           varchar(50) NOT NULL,
+           "amount"             float       NOT NULL,
+           "status"             varchar(20) NOT NULL,
+           "created_at"         TIMESTAMP   NOT NULL DEFAULT now(),
+           "updated_at"         TIMESTAMP   NOT NULL DEFAULT now()
+       )`,
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE "order"
+          ADD CONSTRAINT "UQ_order_order_no" UNIQUE ("order_no")`,
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE "order"
+          ADD CONSTRAINT "UQ_order_user_id_renting_history_id" UNIQUE ("user_id", "renting_history_id")`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "order"`);
+  }
+}
